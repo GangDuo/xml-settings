802fa9d97a99c8df90b96fd2a7e8ebca35f1fe1e
Onoda Yoshiaki <y-onoda@nikki-grp.co.jp>
2018-09-19 13:08:34 +0900

    プロジェクト ファイルを追加。

---
+++ Destination.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace Xml.Settings
+{
+    public class Destination
+    {
+        /**
+         * <?xml version="1.0" encoding="utf-8" ?>
+         * <destinations>
+         *   <destination>p1@example.co.jp</destination>
+         *   <destination>p2@example.co.jp</destination>
+         * </destinations>
+         */
+        public static List<string> Load(string path)
+        {
+            var destinations = new List<string>();
+            using (var sr = new StreamReader(path))
+            {
+                var document = XElement.Load(sr);
+                foreach (var dst in document.Descendants("destination"))
+                {
+                    Debug.WriteLine(dst.Value);
+                    destinations.Add(dst.Value);
+                }
+            }
+            return destinations;
+        }
+    }
+}
---
+++ Xml.Settings.UnitTest/DestinationTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Xml.Settings.UnitTest
+{
+    [TestClass]
+    public class DestinationTest
+    {
+        [TestMethod]
+        public void TestLoad()
+        {
+            string[] actual = new string[] { "p1@example.co.jp", "p2@example.co.jp" };
+            CollectionAssert.AreEqual(Destination.Load(@"..\..\data\destinations.xml"), actual);
+        }
+    }
+}
